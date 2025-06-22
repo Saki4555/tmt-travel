33ff25acaa4c81605cdf631007021083bf4b71b3
saki4555 <saki999v@gmail.com>
2025-06-23 01:28:20 +0600

    Fix: Testimonial Section Smooth scroll issue on mobile devices

--- tmt-client/src/components/home/testimonial/TestimonialCard.jsx
+++ tmt-client/src/components/home/testimonial/TestimonialCard.jsx
@@ -6,37 +6,8 @@ const TestimonialCard = ({ testimonial }) => {
 
   return (
    
-    //   <div className="max-w-md   w-full transition-all group hover:backdrop-blur-xl duration-300 hover:bg-transparent hover:scale-105 mx-auto bg-gray-100 shadow-lg rounded-lg overflow-hidden p-6 border border-gray-200 hover:border-gray-500 flex flex-col h-full">
-      
-    //   {/* Quote Icon */}
-    //   <FaQuoteLeft className="text-[#57B4BA] text-3xl" />
-  
-    //   {/* Review Text */}
-    //   <p className="text-gray-700 group-hover:text-gray-100 italic mt-3 flex-1">
-    //     {review}
-    //   </p>
-  
-    //   {/* User Info & Rating */}
-    //   <div className="flex justify-between items-center mt-4">
-    //     <div>
-    //       <h3 className="text font-semibold group-hover:text-gray-50 text-gray-800">{name}</h3>
-    //       <p className="text-sm text-gray-500 group-hover:text-gray-200 flex items-center">
-    //         <span className="mr-1">{country}</span>
-    //       </p>
-    //     </div>
-  
-    //     {/* Star Rating */}
-    //     <div className="flex items-center">
-    //       {Array.from({ length: 5 }, (_, index) => (
-    //         <FaStar
-    //           key={index}
-    //           className={`text-lg ${index < rating ? "text-[#57B4BA]" : "text-gray-300"}`}
-    //         />
-    //       ))}
-    //     </div>
-    //   </div>
-    // </div>
-    <div className="max-w-md w-full font-jost transition-all group backdrop-blur-3xl bg-white/10 hover:bg-white/15 duration-300 hover:scale-[1.05] mx-auto shadow-lg rounded-xl overflow-hidden p-6 border border-transparent hover:border-[#57B4BA] flex flex-col h-full">
+    
+    <div className="max-w-md w-full font-jost transition-all group lg:backdrop-blur-3xl bg-white/10 hover:bg-white/15 duration-300 lg:hover:scale-[1.05] mx-auto shadow-lg rounded-xl overflow-hidden p-6 border border-transparent hover:border-[#57B4BA] flex flex-col h-full">
       
     {/* Quote Icon */}
     <FaQuoteLeft className="text-slate-600 text-4xl opacity-80" />
